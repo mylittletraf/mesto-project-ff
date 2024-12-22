4ab313b69d2330163d8dd54806c04af91ae4649b
mylittletraf <kanviser@gmail.com>
2024-12-23 02:31:54 +0300

    review 1

--- scripts/index.js
+++ scripts/index.js
@@ -13,36 +13,70 @@ const addButton = document.querySelector(".profile__add-button");
 const saveButton = document.querySelector(".popup__button");
 const popupNewCard = document.querySelector(".popup_type_new-card");
 const closeButton = popupNewCard.querySelector(".popup__close");
+const inputCardName = document.querySelector(".popup__input_type_card-name");
+const inputCardLink = document.querySelector(".popup__input_type_url");
 const cardList = initialCards;
 
-const addCard = (name, link) => {
+
+const handleDelete = (cardElement) => {
+  cardElement.remove();
+};
+
+
+const createCard = (item, handleDelete) => {
   const cardTemplate = document.querySelector(`#card-template`).content;
   const cardElement = cardTemplate
     .querySelector(`.places__item`)
     .cloneNode(true);
-  cardElement.querySelector(".card__image").src = link;
-  cardElement.querySelector(".card__image").alt =
-    "Фото карточки с названием " + name;
-  cardElement.querySelector(".card__title").innerText = name;
+  const cardImage = cardElement.querySelector(".card__image");
+  cardImage.src = item.link;
+  cardImage.alt = `Фото карточки с названием ${item.name}`;
+  cardElement.querySelector(".card__title").textContent = item.name;
 
   cardElement
     .querySelector(".card__delete-button")
     .addEventListener("click", () => {
-      cardElement.remove();
+      handleDelete(cardElement);
     });
 
   cardElement
     .querySelector(".card__like-button")
-    .addEventListener("click", (e) => {
-      e.target.classList.toggle("card__like-button_is-active");
+    .addEventListener("click", (evt) => {
+      evt.target.classList.toggle("card__like-button_is-active");
     });
 
-  placeContainer.append(cardElement);
-};
+  return cardElement;
+}
 
-cardList.forEach((card) => {
-  const cardName = card.name;
-  const cardLink = card.link;
+cardList.forEach((cardItem) => {
+  const newCard = createCard(cardItem, handleDelete);
+  placeContainer.append(newCard);
+});
 
-  addCard(cardName, cardLink);
+
+addButton.addEventListener("click", () => {
+  popupNewCard.classList.add("popup_is-opened");
+});
+
+saveButton.addEventListener("click", () => {
+  const cardName = inputCardName.value;
+  const cardLink = inputCardLink.value;
+
+  const newCardForm = createCard({
+    name: cardName,
+    link: cardLink,
+  },handleDelete);
+  placeContainer.append(newCardForm);
+
+  popupNewCard.classList.toggle("popup_is-opened");
+
+  inputCardName.value = "";
+  inputCardLink.value = "";
+
+});
+
+closeButton.addEventListener("click", () => {
+  popupNewCard.classList.remove("popup_is-opened");
+  inputCardName.value = "";
+  inputCardLink.value = "";
 });
