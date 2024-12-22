632a80977eefd4374a98c25066c25eecfec29edd
mylittletraf <kanviser@gmail.com>
2024-12-23 01:18:42 +0300

    add modal new

--- scripts/index.js
+++ scripts/index.js
@@ -10,6 +10,9 @@
 
 const placeContainer = document.querySelector('.places__list');
 const addButton = document.querySelector('.profile__add-button');
+const saveButton = document.querySelector('.popup__button');
+const popupNewCard = document.querySelector('.popup_type_new-card');
+const closeButton = popupNewCard.querySelector('.popup__close');
 const cardList = initialCards
 
 const addCard = (name, link) => {
@@ -30,12 +33,32 @@ const addCard = (name, link) => {
     placeContainer.append(cardElement);
 }
 
-addButton.addEventListener('click', () => {
-    cardList.forEach(card => {
+cardList.forEach(card => {
         const cardName = card.name;
         const cardLink = card.link;
 
         addCard(cardName, cardLink);
-    })
 })
+
+addButton.addEventListener('click', () => {
+    popupNewCard.classList.add('popup_is-opened');
+});
+
+saveButton.addEventListener('click', () => {
+
+    const cardName = document.querySelector(".popup__input_type_card-name").value;
+    const cardLink = document.querySelector(".popup__input_type_url").value;
+
+    addCard(cardName, cardLink);
+
+    popupNewCard.classList.toggle('popup_is-opened');
+
+    document.querySelector(".popup__input_type_card-name").value = '';
+    document.querySelector(".popup__input_type_url").value = '';
+});
+
+closeButton.addEventListener('click', () => {
+    popupNewCard.classList.remove('popup_is-opened');
+});
+
 
